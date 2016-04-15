4bdd9060c782785e645322b2e959f1e30e83f2cd
Elviro Rocca <elviro.rocca@facile.it>
2016-04-15 15:06:30 +0200

    Initial Commit

---
+++ GoL/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GoL
+//
+//  Created by Elviro Rocca on 15/04/16.
+//  Copyright © 2016 exfalsoquodlibet.it. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
